6bfaceef489eaa09585df6484153d110a59c974d
Auza'i Al Faqih <faqihfaq40@gmail.com>
2025-11-04 20:05:56 +0700

    pertemuan5

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,10 +3,26 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "udah ah cape";
+    }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 5;
+$lingkaran = new LuasLingkaran(5);
+$lingkaran->tampil('roda'); //panggil method
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ". $rumus;
+LuasLingkaran::testing(); //panggil static method
