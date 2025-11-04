c4b38dac3aa2f9ec0c8214c73fa35d66d2c847cc
Auza'i Al Faqih <faqihfaq40@gmail.com>
2025-11-04 20:11:32 +0700

    pertemuan6

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,12 +1,24 @@
 <?php
 
+namespace App\Math; //buat namespace untuk class LuasLingkaran
+
 class LuasLingkaran {
     public const phi = 3.14;
-    public int $jari;
-}
+    private int $jari;
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 5;
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; // 5
+    }
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ". $rumus;
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "udah ah cape";
+    }
